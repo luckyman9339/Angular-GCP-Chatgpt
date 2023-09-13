d48a4ff043dcb9eef1223540c0acf8e794ddc1b8
luckyman9339 <luckyman9339@gmail.com>
2023-09-13 01:31:44 -0400

    Incorporated speech to text functionality

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2, NgZone } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, NgZone, OnDestroy } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SharedServiceService } from '../shared-service.service';
@@ -6,7 +6,9 @@ import { OpenAI } from 'openai'; // request openai
 
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
-const openai = new OpenAI({ 
+declare var SpeechRecognition: any;  // Add this line for speech recognition
+
+const openai = new OpenAI({
   apiKey: environment.OPENAI_API_KEY,
   dangerouslyAllowBrowser: true, // Enable browser-like environment (use with caution)
 });
@@ -24,15 +26,22 @@ const openai = new OpenAI({
     ])
   ]
 })
-export class MainComponent implements OnInit {
-  constructor (
+export class MainComponent implements OnInit, OnDestroy {
+  constructor(
     private sharedService: SharedServiceService,
     private renderer: Renderer2,
-    private ngZone: NgZone
-    ) { }
+    private ngZone: NgZone,
+  ) { }
   @ViewChild('responseCardContainer', { static: false }) responseCardContainer?: ElementRef;
   input: string = '';
   response: string = ''; // initialize response string
+  recognition: any; // for speech recognition
+  model_ai: string = 'Jarvis';
+  send_command: boolean = false; // decides whether or not to send command
+  // user_input: string = ''; // user_input
+  // private _command:string = '';
+  listenCommand: string = '';
+
   generateRandomResponse(): string {
     const responses = [
       'Ask me anything',
@@ -45,13 +54,40 @@ export class MainComponent implements OnInit {
       'What can I help you with today?',
       'Hi! What would you like to ask?'
     ];
-  
+
     // Pick a random response from the array
     const randomIndex = Math.floor(Math.random() * responses.length);
     return responses[randomIndex];
   }
   ngOnInit(): void {
-      this.response = this.generateRandomResponse();
+    this.response = this.generateRandomResponse();
+    // Initialize speech recognition
+    this.recognition = new (window as any).webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.recognition.lang = 'en-US'; // 'en-US' for English. Use 'zh-CN' for Chinese.
+
+    this.recognition.onresult = (event: any) => {
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+          const transcript = event.results[i][0].transcript.trim();
+          if (this.send_command === true) {
+            console.log('Final transcript: ', transcript); // transcript that is final (that should be submitted to chat)
+            // place this.chat here
+            this.send_command = false;
+          }
+        } else {
+          const transcript = event.results[i][0].transcript.trim();
+          // this.listenCommand = transcript;
+          if (this.model_ai.toLowerCase() === transcript.toLowerCase()) { // if value is equal to name of model_ai
+            console.log('I hear you loud and clear sir');
+            this.send_command = true;
+          }
+          // console.log('Interim transcript: ', transcript); // transcript that is constantly being typed out
+        }
+      }
+    };
+    this.recognition.start();
   }
   // Function to simulate typing animation
   typeResponse(response: string) {
@@ -62,7 +98,7 @@ export class MainComponent implements OnInit {
       if (index < response.length) {
         this.response += response.charAt(index);
         index++;
-        
+
         // Use NgZone to make sure the UI gets updated
         this.ngZone.run(() => {
           if (this.responseCardContainer) {
@@ -80,8 +116,8 @@ export class MainComponent implements OnInit {
   }
 
   // API function call
-  async chat (prompt: string, ai: string) {
-    try{
+  async chat(prompt: string, ai: string) {
+    try {
       const response = await this.sharedService.openaiChat(prompt, ai);
       this.response = response.choices[0].message.content;
       // Trigger the typing animation
@@ -90,7 +126,7 @@ export class MainComponent implements OnInit {
       console.log("Something went wrong in chat API call", error);
     }
   }
-  
+
   // Chatbox for OpenAI
   chatInput(event: Event) {
     this.input = (event.target as HTMLInputElement).value;
@@ -98,7 +134,10 @@ export class MainComponent implements OnInit {
 
   submit() {
     // console.log('This is the input value:', this.input);
-    this.chat(this.input, 'Jarvis');
+    this.chat(this.input, this.model_ai);
   }
-
+  // Destroy the speech recognition element
+  ngOnDestroy(): void {
+    this.recognition.stop();
+  }
 }
